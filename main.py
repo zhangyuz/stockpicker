2a45e774a3c7bcffa900884a6a696b10d111e185
Zhang Yuzheng <me@zhangyuzheng.com>
2018-04-21 14:41:37 +0800

    Main: init: add entry point for app and python code template
    Signed-off-by: Zhang Yuzheng <me@zhangyuzheng.com>

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' Entry Point '
+
+__author__ = 'Zhang YuZ'
+
+if __name__ == '__main__':
+    from stockpicker.stockpicker import StockPicker
+    picker = StockPicker()
+    picker.run()
---
+++ stockpicker/stockpicker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' Entry for stock picker '
+
+__author__ = 'Zhang YuZ'
+
+class StockPicker(object):
+    def __init__(self):
+        print('StockPicker initialized!!!')
+
+    def run(self):
+        print("running!!!")
+
